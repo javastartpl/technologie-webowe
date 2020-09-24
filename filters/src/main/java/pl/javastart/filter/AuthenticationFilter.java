1ca026427e1f66f3b8b814d730ef85725c5b7fb2
slawek <slawekwch@gmail.com>
2020-09-24 15:23:48 +0200

    added segments, web-inf and filters

---
+++ filters/src/main/java/pl/javastart/filter/AuthenticationFilter.java
@@ -0,0 +1,20 @@
+package pl.javastart.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+//@WebFilter("/secret")
+public class AuthenticationFilter implements Filter {
+    private static final String PASS = "h@rdPass";
+
+    public void doFilter(ServletRequest reqest, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String pass = reqest.getParameter("pass");
+        if (pass != null && pass.equals(PASS))
+            chain.doFilter(reqest, response);
+        else
+            ((HttpServletResponse)response).sendError(403, "Wrong password");
+    }
+}
---
+++ segments_hiding/src/main/java/pl/javastart/HomeController.java
@@ -0,0 +1,18 @@
+package pl.javastart;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@WebServlet("")
+public class HomeController extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<String> products = Arrays.asList("sukienki", "torebki", "spodnie", "buty");
+        request.setAttribute("products", products);
+        request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+    }
+}
